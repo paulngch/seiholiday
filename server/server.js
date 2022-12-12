4e2228c86af77133cf8e6a3513f30406eb0fe602
Your Name <you@example.com>
2022-12-12 15:58:30 +0800

    MONGO_URI added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
 require("dotenv").config();
 const morgan = require("morgan");
